941ced2517870348386e5f155440c680d2211b17
Joy Chu <joy_chu@wistron.com>
2019-07-17 14:54:29 +0800

    Revert "Revert "Modify copyright statement in mihawk platform file""

--- platforms/astbmc/mihawk.c
+++ platforms/astbmc/mihawk.c
@@ -1,4 +1,5 @@
-/* Copyright 2017 IBM Corp.
+/* Copyright 2019 Wistron Corp.
+ * Copyright 2017 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
